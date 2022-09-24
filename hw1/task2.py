034dbb2ee9faf824f35a51bafc69d20dc0710a62
Gogol Egor <pro100egar@gmail.com>
2022-09-24 19:15:50 +0700

    hw1_add

---
+++ hw1/task2.py
@@ -0,0 +1,25 @@
+#Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений
+#предикат.
+
+
+def inpNum(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def checkPred(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = inpNum(3)
+
+if checkPred(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
